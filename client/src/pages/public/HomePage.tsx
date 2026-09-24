2bb17fc8c5729c3868d51746c004c028ad7f1a7a
Bhuwan Upadhyay <bhuwan.upadhyay.work@gmail.com>
2026-09-24 16:49:14 +0530

    feat: hompage update and package.json

--- client/src/pages/public/HomePage.tsx
+++ client/src/pages/public/HomePage.tsx
@@ -1,11 +1,21 @@
+import CategorySection from "@/components/CategorySection";
+import SongFeaturedSection from "@/components/SongFeaturedSection";
 
 const HomePage = () => {
-
+  // const {data} = useSongById('6ab4b82c5a75e2a7a667889f')
+  // const {data} = useSong(1,'');
   return (
-    <div className="">
-     
-    </div>
-  )
-}
+    <main className="w-full min-w-0 space-y-2">
+      <CategorySection />
+      <SongFeaturedSection/>
+      {/* <div className="flex flex-col gap-2">
+        <SongHorizontalCard song={data}/>
+        <SongHorizontalCard song={data}/>
+        <SongHorizontalCard song={data}/>
+        <SongHorizontalCard song={data}/>
+      </div> */}
+    </main>
+  );
+};
 
-export default HomePage
+export default HomePage;
